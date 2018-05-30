0e4d5bab183f71b8601ab6f65c1d816450fed549
HiP <jeanphilippe.baconnais@gmail.com>
2018-05-31 00:52:38 +0200

    Add ex1

---
+++ src/main/ex1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("\nExercice 1 : ")
+	fmt.Print("\nHouston we just make a simple hello to the Earth")
+	fmt.Print("\n\n")
+}
---
+++ src/z_ex2/ex2.go
@@ -0,0 +1,9 @@
+package z_ex2
+
+import "fmt"
+
+func main() {
+	fmt.Print("\nExercice 2 : ")
+	fmt.Print("\nHouston we just make a simple hello to the Earth")
+	fmt.Print("\n\n")
+}
